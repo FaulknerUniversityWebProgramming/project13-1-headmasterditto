bd3b013010592e5cb715b99b393edf2692e539ad
Micah Green <m.green.x13@live.com>
2018-11-07 21:38:34 -0600

    Step 3 done
    I think I did this right?

--- TravelPhoto.class.php
+++ TravelPhoto.class.php
@@ -7,6 +7,14 @@
 		private $latitude;
 		private $longitude;
 		private $ID;
-		static $photoID;
+		static $photoID = 0;
+		function __construct($name_in,$title_in,$desc_in,$lat_in,$long_in){
+			$this->fileName = $name_in;
+			$this->title = $title_in;
+			$this->description = $desc_in;
+			$this->latitude = $lat_in;
+			$this->longitude = $long_in;
+			self::$photoID++;
+		};
 	}
 ?>
